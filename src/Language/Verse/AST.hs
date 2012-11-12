f794edf5057b7b21704d9861da936e929e8c87ad
P.C. Shyamshankar <sykora@lucentbeing.com>
2012-11-12 12:49:42 -0500

    Add an initial version of the Verse AST.
    * Currently, the AST only contains paragraphs (vanilla blocks) and
      content (vanilla inline) elements.

---
+++ src/Language/Verse/AST.hs
@@ -0,0 +1,22 @@
+-- | Representation of a Verse document.
+module Language.Verse.AST (
+    Document(..),
+    Block(..),
+    Inline(..)
+) where
+
+-- | A 'Document' is a list of blocks.
+data Document
+    = Document [Block]
+  deriving (Eq, Read, Show)
+
+-- | A 'Block' is a top-level document element, typically separated by newlines.
+data Block
+    = Paragraph [Inline] -- ^ A block-level element with no markup over that of its constituent elements.
+  deriving (Eq, Read, Show)
+
+-- | An 'Inline' is a piece of text, usually separated by syntactic delimiters, many of which can
+-- occur side-by-side inside a block-level element.
+data Inline
+    = Content String -- ^ A non-marked-up piece of text.
+  deriving (Eq, Read, Show)
