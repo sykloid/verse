36422e6f4143afe419392a1d3196cc7858348deb
P.C. Shyamshankar <sykora@lucentbeing.com>
2012-11-12 13:25:40 -0500

    Add type signatures for basic parsers.

--- src/Language/Verse/Parser.hs
+++ src/Language/Verse/Parser.hs
@@ -1,13 +1,39 @@
 -- | A parser for Verse syntax.
 module Language.Verse.Parser (
     Parser,
-    runParser
+    runParser,
+
+    document,
+
+    block,
+    paragraph,
+
+    inline,
+    content,
 ) where
 
 import Control.Monad.Identity
+
 import Text.Parsec (ParsecT, SourceName, ParseError, runParserT)
+
+import Language.Verse.AST
 
 type Parser = ParsecT String () Identity
 
 runParser :: Parser a -> SourceName -> String -> Either ParseError a
 runParser p s i = runIdentity $ runParserT p () s i
+
+document :: Parser Document
+document = undefined
+
+block :: Parser Block
+block = undefined
+
+paragraph :: Parser Block
+paragraph = undefined
+
+inline :: Parser Inline
+inline = undefined
+
+content :: Parser Inline
+content = undefined
